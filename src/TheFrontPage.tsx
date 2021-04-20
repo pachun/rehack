6be8e3a6d1d2d651b6bc9bd905f922f4e6144009
Nick Pachulski <nick@pachulski.me>
2021-04-20 15:26:56 -0400

    wip

--- src/TheFrontPage.tsx
+++ src/TheFrontPage.tsx
@@ -30,26 +30,15 @@ const isFirstFrontPageLoadOrHasBeenFiveMinutesSinceLastFrontPageRefresh = async 
 
 const numberOfStoriesPerPage = 20
 
-const getTheFrontPageStories = async (): Promise<HackerNewsItem[]> => {
+const getTheFrontPageStoriesIds = async (): Promise<number[]> => {
   const frontPageStoryIdsRequest = await fetch(
     `https://hacker-news.firebaseio.com/v0/topstories.json`,
   )
-  const frontPageStoryIds: number[] = (
-    await frontPageStoryIdsRequest.json()
-  ).slice(0, numberOfStoriesPerPage)
-  const topStories = Promise.all(
-    frontPageStoryIds.map(
-      (frontPageStoryId): Promise<HackerNewsItem> => {
-        return fetch(
-          `https://hacker-news.firebaseio.com/v0/item/${frontPageStoryId}.json`,
-        ).then(frontPageStoryRequest =>
-          frontPageStoryRequest.json().then(frontPageStory => frontPageStory),
-        )
-      },
-    ),
-  )
-  return await topStories
+  const frontPageStoryIds: number[] = await frontPageStoryIdsRequest.json()
+  return frontPageStoryIds
 }
+
+const whenScrolledToWithinHalfTheScreensHeightFromTheBottom = 0.5
 
 const TheFrontPage = ({
   navigation,
@@ -63,19 +52,57 @@ const TheFrontPage = ({
     setIsRefreshingTheFrontPage,
   ] = React.useState(false)
 
+  const [numberOfPagesLoaded, setNumberOfPagesLoaded] = React.useState<number>(
+    0,
+  )
+  const [theFrontPageStoriesIds, setTheFrontPageStoriesIds] = React.useState<
+    number[]
+  >([])
+
   const [theFrontPageStories, setTheFrontPageStories] = React.useState<
     HackerNewsItem[]
   >([])
 
-  const refreshTheFrontPage = async () => {
+  const getNextPageOfFrontPageStories = React.useCallback(
+    async (page: number = numberOfPagesLoaded) => {
+      const positionOfFirstStoriesId = page * numberOfStoriesPerPage
+      const idsOfStoriesOnNextPage = theFrontPageStoriesIds.slice(
+        positionOfFirstStoriesId,
+        positionOfFirstStoriesId + numberOfStoriesPerPage,
+      )
+      const nextPageOfStories = await Promise.all(
+        idsOfStoriesOnNextPage.map(
+          (idOfStoryOnNextPage): Promise<HackerNewsItem> => {
+            return fetch(
+              `https://hacker-news.firebaseio.com/v0/item/${idOfStoryOnNextPage}.json`,
+            ).then(storyOnNextPageRequest =>
+              storyOnNextPageRequest
+                .json()
+                .then(storyOnNextPage => storyOnNextPage),
+            )
+          },
+        ),
+      )
+      const allStories =
+        page === 0
+          ? nextPageOfStories
+          : [...theFrontPageStories, ...nextPageOfStories]
+      setTheFrontPageStories(allStories)
+      setNumberOfPagesLoaded(allStories.length / numberOfStoriesPerPage)
+    },
+    [numberOfPagesLoaded, theFrontPageStories, theFrontPageStoriesIds],
+  )
+
+  const refreshTheFrontPage = React.useCallback(async () => {
     AsyncStorage.setItem(
       "Last ISO8601 Front Page Refresh Time",
       DateTime.now().toString(),
     )
     setIsRefreshingTheFrontPage(true)
-    setTheFrontPageStories(await getTheFrontPageStories())
+    setTheFrontPageStoriesIds(await getTheFrontPageStoriesIds())
+    getNextPageOfFrontPageStories(0)
     setIsRefreshingTheFrontPage(false)
-  }
+  }, [getNextPageOfFrontPageStories])
 
   React.useEffect(() => {
     const unsubscribe = navigation.addListener("focus", async () => {
@@ -87,12 +114,16 @@ const TheFrontPage = ({
       }
     })
     return unsubscribe
-  }, [navigation, theFrontPageStories.length])
+  }, [navigation, theFrontPageStories.length, refreshTheFrontPage])
 
   return (
     <>
       <View style={{ backgroundColor: "#fff", height: insets.top }} />
       <FlatList
+        onEndReachedThreshold={
+          whenScrolledToWithinHalfTheScreensHeightFromTheBottom
+        }
+        onEndReached={getNextPageOfFrontPageStories}
         onRefresh={refreshTheFrontPage}
         refreshing={isRefreshingTheFrontPage}
         style={{ backgroundColor: "#fff" }}
